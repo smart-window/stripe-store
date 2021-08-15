a298ac5df1c7b72284839ef65e284514369b955e
smart-window <smart.free.dev@gmail.com>
2021-08-16 01:30:28 +0530

    payment related code and project structure refactoring

---
+++ pages/api/user.ts
@@ -0,0 +1,7 @@
+
+
+
+
+export const userData = (request, response) => {
+    response.status(200);
+}
---
+++ pages/result.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { fetchGetJSON } from '../utils/api-helpers'
+import useSWR from 'swr'
+
+const ResultPage: NextPage = () => {
+  const router = useRouter()
+
+  const { data, error } = useSWR(
+    router.query.session_id
+      ? `/api/checkout_sessions/${router.query.session_id}`
+      : null,
+    fetchGetJSON
+  )
+
+  if (error) return <div>failed to load</div>
+
+  return (
+    <div className="page-container">
+        <h1>Checkout Payment Result</h1>
+        <h2>Status: {data?.payment_intent?.status ?? 'loading...'}</h2>
+     </div>
+  )
+}
+
+export default ResultPage
